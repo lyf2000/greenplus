b31be948a59df042ff62911e01945598739d34cb
lyf2000 <lyf2000@mail.ru>
2020-05-21 12:01:03 +0300

    false

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^04kwkw#f-#*6a3=o=my)!ifk8gnnoie6$yn0*kgg576guyr-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
